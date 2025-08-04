e979d7a8f5761bb84a07c88034d7b016be6b2115
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-04 11:41:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i = 0; i < str.length; i++){
+		let flag = false;
+		for(let j = 0; j < str.length; j++){
+			if(str[i] == str[j] && i != j){
+				flag = true;
+				break;
+			}
+		}
+
+		if(flag == false) return str[i];
+	}
+
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
